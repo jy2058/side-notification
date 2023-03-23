77ca3786777d6ccd41e72197da838be3da2e8827
jy2058 <jiy3285@gmail.com>
2023-03-23 21:54:21 +0900

    Initial commit

---
+++ src/main/java/com/wside/notification/controller/NotificationController.java
@@ -0,0 +1,10 @@
+package com.wside.notification.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("value=api/v1/organization/{organizationId}/license")
+public class NotificationController {
+
+}
